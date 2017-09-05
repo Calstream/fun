76ae4b3c912972cba239ce7ce073a8333240c64e
Calstream <calstream.lgn@gmail.com>
2017-09-05 10:29:17 +0300

    2

--- fun/Form1.Designer.cs
+++ fun/Form1.Designer.cs
@@ -28,113 +28,124 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-			System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-			System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.textBox2 = new System.Windows.Forms.TextBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			this.button1 = new System.Windows.Forms.Button();
-			this.comboBox1 = new System.Windows.Forms.ComboBox();
-			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			this.label_from = new System.Windows.Forms.Label();
+			this.label_to = new System.Windows.Forms.Label();
+			this.button_plot = new System.Windows.Forms.Button();
+			this.comboBox_fun = new System.Windows.Forms.ComboBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label_fun = new System.Windows.Forms.Label();
+			this.numericUpDown_from = new System.Windows.Forms.NumericUpDown();
+			this.numericUpDown_to = new System.Windows.Forms.NumericUpDown();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown_from)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown_to)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// chart1
+			// label_from
 			// 
-			chartArea1.Name = "ChartArea1";
-			this.chart1.ChartAreas.Add(chartArea1);
-			legend1.Name = "Legend1";
-			this.chart1.Legends.Add(legend1);
-			this.chart1.Location = new System.Drawing.Point(0, 0);
-			this.chart1.Name = "chart1";
-			series1.ChartArea = "ChartArea1";
-			series1.Legend = "Legend1";
-			series1.Name = "Series1";
-			this.chart1.Series.Add(series1);
-			this.chart1.Size = new System.Drawing.Size(438, 371);
-			this.chart1.TabIndex = 0;
-			this.chart1.Text = "chart1";
+			this.label_from.AutoSize = true;
+			this.label_from.Location = new System.Drawing.Point(446, 15);
+			this.label_from.Name = "label_from";
+			this.label_from.Size = new System.Drawing.Size(18, 13);
+			this.label_from.TabIndex = 3;
+			this.label_from.Text = "x1";
 			// 
-			// textBox1
+			// label_to
 			// 
-			this.textBox1.Location = new System.Drawing.Point(487, 12);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(100, 20);
-			this.textBox1.TabIndex = 1;
+			this.label_to.AutoSize = true;
+			this.label_to.Location = new System.Drawing.Point(444, 45);
+			this.label_to.Name = "label_to";
+			this.label_to.Size = new System.Drawing.Size(18, 13);
+			this.label_to.TabIndex = 4;
+			this.label_to.Text = "x2";
 			// 
-			// textBox2
+			// button_plot
 			// 
-			this.textBox2.Location = new System.Drawing.Point(487, 38);
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(100, 20);
-			this.textBox2.TabIndex = 2;
+			this.button_plot.Location = new System.Drawing.Point(476, 310);
+			this.button_plot.Name = "button_plot";
+			this.button_plot.Size = new System.Drawing.Size(75, 23);
+			this.button_plot.TabIndex = 5;
+			this.button_plot.Text = "Plot";
+			this.button_plot.UseVisualStyleBackColor = true;
 			// 
-			// label1
+			// comboBox_fun
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(446, 15);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(35, 13);
-			this.label1.TabIndex = 3;
-			this.label1.Text = "label1";
+			this.comboBox_fun.FormattingEnabled = true;
+			this.comboBox_fun.Items.AddRange(new object[] {
+            "SIN",
+            "COS",
+            "SQRT",
+            "X^2",
+            "X^3"});
+			this.comboBox_fun.Location = new System.Drawing.Point(457, 117);
+			this.comboBox_fun.Name = "comboBox_fun";
+			this.comboBox_fun.Size = new System.Drawing.Size(121, 21);
+			this.comboBox_fun.TabIndex = 6;
 			// 
-			// label2
+			// pictureBox1
 			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(444, 45);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(35, 13);
-			this.label2.TabIndex = 4;
-			this.label2.Text = "label2";
+			this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 8);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(426, 350);
+			this.pictureBox1.TabIndex = 7;
+			this.pictureBox1.TabStop = false;
 			// 
-			// button1
+			// label_fun
 			// 
-			this.button1.Location = new System.Drawing.Point(476, 310);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 5;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
+			this.label_fun.AutoSize = true;
+			this.label_fun.Location = new System.Drawing.Point(464, 101);
+			this.label_fun.Name = "label_fun";
+			this.label_fun.Size = new System.Drawing.Size(48, 13);
+			this.label_fun.TabIndex = 8;
+			this.label_fun.Text = "Function";
 			// 
-			// comboBox1
+			// numericUpDown_from
 			// 
-			this.comboBox1.FormattingEnabled = true;
-			this.comboBox1.Location = new System.Drawing.Point(466, 90);
-			this.comboBox1.Name = "comboBox1";
-			this.comboBox1.Size = new System.Drawing.Size(121, 21);
-			this.comboBox1.TabIndex = 6;
+			this.numericUpDown_from.Location = new System.Drawing.Point(470, 13);
+			this.numericUpDown_from.Name = "numericUpDown_from";
+			this.numericUpDown_from.Size = new System.Drawing.Size(108, 20);
+			this.numericUpDown_from.TabIndex = 10;
+			// 
+			// numericUpDown_to
+			// 
+			this.numericUpDown_to.Location = new System.Drawing.Point(470, 39);
+			this.numericUpDown_to.Name = "numericUpDown_to";
+			this.numericUpDown_to.Size = new System.Drawing.Size(108, 20);
+			this.numericUpDown_to.TabIndex = 11;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(599, 370);
-			this.Controls.Add(this.comboBox1);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.textBox2);
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.chart1);
+			this.Controls.Add(this.numericUpDown_to);
+			this.Controls.Add(this.numericUpDown_from);
+			this.Controls.Add(this.label_fun);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.comboBox_fun);
+			this.Controls.Add(this.button_plot);
+			this.Controls.Add(this.label_to);
+			this.Controls.Add(this.label_from);
 			this.Name = "Form1";
 			this.Text = "Form1";
-			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown_from)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown_to)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
 		}
 
 		#endregion
-
-		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-		private System.Windows.Forms.TextBox textBox1;
-		private System.Windows.Forms.TextBox textBox2;
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label_from;
+		private System.Windows.Forms.Label label_to;
+		private System.Windows.Forms.Button button_plot;
+		private System.Windows.Forms.ComboBox comboBox_fun;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label_fun;
+		private System.Windows.Forms.NumericUpDown numericUpDown_from;
+		private System.Windows.Forms.NumericUpDown numericUpDown_to;
 	}
 }
 
