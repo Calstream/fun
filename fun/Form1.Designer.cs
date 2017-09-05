7a3e60181926c90c528f1d4a18fa22765c87eaad
Calstream <calstream.lgn@gmail.com>
2017-09-05 11:02:36 +0300

    3

--- fun/Form1.Designer.cs
+++ fun/Form1.Designer.cs
@@ -32,11 +32,12 @@
 			this.label_to = new System.Windows.Forms.Label();
 			this.button_plot = new System.Windows.Forms.Button();
 			this.comboBox_fun = new System.Windows.Forms.ComboBox();
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox = new System.Windows.Forms.PictureBox();
 			this.label_fun = new System.Windows.Forms.Label();
 			this.numericUpDown_from = new System.Windows.Forms.NumericUpDown();
 			this.numericUpDown_to = new System.Windows.Forms.NumericUpDown();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.button_clear = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown_from)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown_to)).BeginInit();
 			this.SuspendLayout();
@@ -61,12 +62,13 @@
 			// 
 			// button_plot
 			// 
-			this.button_plot.Location = new System.Drawing.Point(476, 310);
+			this.button_plot.Location = new System.Drawing.Point(481, 302);
 			this.button_plot.Name = "button_plot";
 			this.button_plot.Size = new System.Drawing.Size(75, 23);
 			this.button_plot.TabIndex = 5;
 			this.button_plot.Text = "Plot";
 			this.button_plot.UseVisualStyleBackColor = true;
+			this.button_plot.Click += new System.EventHandler(this.button_plot_Click);
 			// 
 			// comboBox_fun
 			// 
@@ -82,14 +84,15 @@
 			this.comboBox_fun.Size = new System.Drawing.Size(121, 21);
 			this.comboBox_fun.TabIndex = 6;
 			// 
-			// pictureBox1
+			// pictureBox
 			// 
-			this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-			this.pictureBox1.Location = new System.Drawing.Point(12, 8);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(426, 350);
-			this.pictureBox1.TabIndex = 7;
-			this.pictureBox1.TabStop = false;
+			this.pictureBox.BackColor = System.Drawing.SystemColors.Control;
+			this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox.Location = new System.Drawing.Point(12, 8);
+			this.pictureBox.Name = "pictureBox";
+			this.pictureBox.Size = new System.Drawing.Size(426, 350);
+			this.pictureBox.TabIndex = 7;
+			this.pictureBox.TabStop = false;
 			// 
 			// label_fun
 			// 
@@ -114,22 +117,33 @@
 			this.numericUpDown_to.Size = new System.Drawing.Size(108, 20);
 			this.numericUpDown_to.TabIndex = 11;
 			// 
+			// button_clear
+			// 
+			this.button_clear.Location = new System.Drawing.Point(481, 335);
+			this.button_clear.Name = "button_clear";
+			this.button_clear.Size = new System.Drawing.Size(75, 23);
+			this.button_clear.TabIndex = 12;
+			this.button_clear.Text = "Clear";
+			this.button_clear.UseVisualStyleBackColor = true;
+			this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(599, 370);
+			this.Controls.Add(this.button_clear);
 			this.Controls.Add(this.numericUpDown_to);
 			this.Controls.Add(this.numericUpDown_from);
 			this.Controls.Add(this.label_fun);
-			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.pictureBox);
 			this.Controls.Add(this.comboBox_fun);
 			this.Controls.Add(this.button_plot);
 			this.Controls.Add(this.label_to);
 			this.Controls.Add(this.label_from);
 			this.Name = "Form1";
-			this.Text = "Form1";
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.Text = "Plot";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown_from)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown_to)).EndInit();
 			this.ResumeLayout(false);
@@ -142,10 +156,11 @@
 		private System.Windows.Forms.Label label_to;
 		private System.Windows.Forms.Button button_plot;
 		private System.Windows.Forms.ComboBox comboBox_fun;
-		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox;
 		private System.Windows.Forms.Label label_fun;
 		private System.Windows.Forms.NumericUpDown numericUpDown_from;
 		private System.Windows.Forms.NumericUpDown numericUpDown_to;
+		private System.Windows.Forms.Button button_clear;
 	}
 }
 
--- fun/Form1.cs
+++ fun/Form1.cs
@@ -16,5 +16,45 @@ namespace fun
 		{
 			InitializeComponent();
 		}
+
+		private void button_plot_Click(object sender, EventArgs e)
+		{
+			if (numericUpDown_from.Value == numericUpDown_to.Value)
+			{
+				string message = "x1 == x2";
+				DialogResult result = MessageBox.Show(message, "Plot", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+
+			else if (comboBox_fun.SelectedItem == null)
+			{
+				string message = "no func";
+				DialogResult result = MessageBox.Show(message, "Plot", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+
+			else
+			{
+				int w = pictureBox.Width;
+				int h = pictureBox.Height;
+				Bitmap im = new Bitmap(w, h);
+				Graphics img = Graphics.FromImage(im);
+				img.FillRectangle(Brushes.White, 0, 0, w, h);
+				pictureBox.Image = im;
+
+				this.Text = comboBox_fun.Text;
+			}
+		}
+
+		private void button_clear_Click(object sender, EventArgs e)
+		{
+			if (pictureBox.Image != null)
+			{
+				pictureBox.Image.Dispose();
+				pictureBox.Image = null;
+			}
+			numericUpDown_from.Value = 0;
+			numericUpDown_to.Value = 0;
+			comboBox_fun.SelectedItem = null;
+				this.Text = "Plot";
+		}
 	}
 }
